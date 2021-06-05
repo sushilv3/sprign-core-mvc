06979a669fb04609c97fef53b819fed137892684
sushilv3 <sushilv3rma@gmail.com>
2021-06-05 18:29:33 +0530

    added version property

--- src/main/java/com/spring/guru/domain/Product.java
+++ src/main/java/com/spring/guru/domain/Product.java
@@ -2,9 +2,22 @@ package com.spring.guru.domain;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
 public class Product {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Version
+	private Integer version;
+	
 	private String description;
 	private BigDecimal price;
 	private String imageUrl;
@@ -58,6 +71,18 @@ public class Product {
 
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
+	}
+
+
+
+	public Integer getVersion() {
+		return version;
+	}
+
+
+
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 
 
